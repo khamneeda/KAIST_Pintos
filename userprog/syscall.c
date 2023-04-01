0137aa4d297664cb5254310a9c2c2fc9771bbb3f
Haejune <84277606+khamneeda@users.noreply.github.com>
2023-04-01 16:12:43 +0900

    Add skeleton code in syscall_handler (2-2)

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -37,14 +37,92 @@ syscall_init (void) {
 			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
 
-/* The main system call interface */
+/* The main system call interface */	
 void
 syscall_handler (struct intr_frame *f) {
 	// TODO: Your implementation goes here.
 	if(!check_address(f->rsp)){
 		printf("not\n");
 	}
-	printf ("system call!\n");
+	
+	/* Get arguments */
+	uint64_t args[7];
+    args[0]= f->R.rax;
+    args[1]= f->R.rdi;
+    args[2]= f->R.rsi;
+    args[3]= f->R.rdx;
+    args[4]= f->R.r10;
+    args[5]= f->R.r8;
+    args[6]= f->R.r9;
+
+	/* 	Switch cases 
+	Call functions for each cases due to the system call number.
+	Get return value of function if it has, and update rax in IF.
+	Use int64_t for updating rax to match the type of ret in syscall.
+	*/
+	switch (f->R.rax){
+		case SYS_HALT:
+			sys_halt(args);
+			break;		
+		case SYS_EXIT:
+			sys_exit(args);
+			break;		
+		case SYS_FORK:
+			int64_t update = sys_fork(args);
+			break;		
+		case SYS_EXEC:
+			int64_t update = sys_exec(args);
+			break;		
+		case SYS_WAIT:
+			int64_t update = sys_wait(args);
+			break;		
+		case SYS_CREATE:
+			int64_t update = sys_create(args);
+			break;		
+		case SYS_REMOVE:
+			int64_t update = sys_remove(args);
+			break;		
+		case SYS_OPEN:
+			int64_t update = sys_opne(args);
+			break;		
+		case SYS_FILESIZE:
+			int64_t update = sys_filesize(args);
+			break;		
+		case SYS_READ:
+			int64_t update = sys_read(args);
+			break;		
+		case SYS_WRITE:
+			int64_t update = sys_write(args);
+			break;		
+		case SYS_SEEK:
+			sys_seek(args);
+			break;		
+		case SYS_TELL:
+			int64_t update = sys_tell(args);
+			break;		
+		case SYS_CLOSE:
+			sys_close(args);
+			break;		
+
+		/* For project2 Extra*/		
+		// case SYS_MOUNT
+		// case SYS_UMOUNT
+
+
+		/* For project 3, 4 each
+		SYS_MMAP
+		SYS_MUNMAP
+
+		SYS_CHDIR
+		SYS_MKDIR
+		SYS_READDIR
+		SYS_ISDIR
+		SYS_INUMBER
+		SYS_SYMLINK
+		SYS_DUP2
+		*/
+
+	}
 	thread_exit ();
 }
 
@@ -60,4 +138,61 @@ check_address(uintptr_t f){
 		}
 	}
 	return 0;  
+}
+
+
+void
+sys_halt (uint64_t args) {
+}
+
+void
+sys_exit (uint64_targs) {
+}
+
+int64_t
+sys_fork (uint64_t args){
+}
+
+int64_t
+sys_exec (uint64_targs) {
+}
+
+int64_t
+sys_wait (uint64_t args) {
+}
+
+int64_t
+sys_create (uint64_targs) {
+}
+
+int64_t
+sys_remove (uint64_t args) {
+}
+
+int64_t
+sys_open (uint64_targs) {
+}
+
+int64_t
+sys_filesize (uint64_t args) {
+}
+
+int64_t
+sys_read (uint64_targs) {
+}
+
+int64_t
+sys_write (uint64_t args) {
+}
+
+void
+sys_seek (uint64_targs) {
+}
+
+int64_t
+sys_tell (uint64_t args) {
+}
+
+void
+sys_close (uint64_targs) {
 }
